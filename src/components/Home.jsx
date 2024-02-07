8a1062538796fadaee0ab8390e8811b668d0c7c4
halurabiyyu <halurabiyyu@gmail.com>
2024-02-07 22:50:03 +0700

    testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,9 +48,9 @@ const Home = () => {
             />
           </div>
         </div>
-        <div className="flex justify-center items-center">
+        {/* <div className="flex justify-center items-center">
           <img src={ProfileImg} alt="" className="h-[500px] rounded-full   "/>
-        </div>
+        </div> */}
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0013ba" fill-opacity="1" d="M0,192L48,213.3C96,235,192,277,288,272C384,267,480,213,576,192C672,171,768,181,864,197.3C960,213,1056,235,1152,240C1248,245,1344,235,1392,229.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
     </div>
